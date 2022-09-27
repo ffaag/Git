50596b2952928b87893c122e200c13ee8ba8bdd7
xiaofang <1961306389@qq.com>
2022-09-27 16:25:58 +0800

    hotfix commit

--- src/main/java/com/it/git/GitTest.java
+++ src/main/java/com/it/git/GitTest.java
@@ -10,6 +10,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
+        System.out.println("hello git2");
+        System.out.println("hello git44444444444");
     }
 
 
