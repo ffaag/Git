7576c7f468d059d069302cb885be21dba72481f6
xiaofang <1961306389@qq.com>
2022-09-27 16:17:32 +0800

    first commit

---
+++ src/main/java/com/it/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.it.git;
+
+/**
+ * @author ZuYingFang
+ * @time 2022-09-27 16:14
+ * @description
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+
+}
